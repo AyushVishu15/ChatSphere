dc6741fff10cd0195664982602b060e258acaa53
Ayush <168955117+AyushVishu15@users.noreply.github.com>
2025-08-06 00:32:13 +0530

    Update config.js

--- frontend-modern/src/config.js
+++ frontend-modern/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
-export const SOCKET_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://chatsphere-4nyh.onrender.com';
+export const SOCKET_URL = process.env.REACT_APP_API_URL || 'https://chatsphere-4nyh.onrender.com';
